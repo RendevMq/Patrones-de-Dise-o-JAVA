f90a5747e418cd95118eb038789468b1ba1d5a4f
RendevMq <rensystem73@gmail.com>
2024-08-03 14:43:06 -0500

    builder

---
+++ a03builder/src/main/java/com/example/IBuilder.java
@@ -0,0 +1,5 @@
+package com.example;
+
+public interface IBuilder<T> {
+    public T build(); //se tiene que encargar de crear todos los objetos
+}
---
+++ a03builder/src/main/java/com/example/Main.java
@@ -0,0 +1,38 @@
+package com.example;
+
+import com.example.domain.Address;
+import com.example.domain.Contact;
+import com.example.domain.Phone;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        // Employee employee = new Employee();
+
+        // Address address = new Address();
+        // employee.setAddress(address);
+
+        // //y asi crear los demas objetos.. la idea es evitarlo
+
+        //El metodo build es el que se encarga de tomar todo y juntarlo en un 
+        //solo objeto.. siempre que veamos una estructura similar significa que
+        //se esta implementando el patro de diseno builder
+        Employee employee = new Employee.EmployeeBuilder()
+                            .setAge(24)
+                            .setName("Santiago")
+                            .setGender("Male")
+
+                            .setAdress(new Address("Av. Lima 4545" , "Lima" , "Peru" , "15663"))
+                            .setAdress("Av. brasil 4545" , "Lima" , "Peru" , "45121")
+
+                            .setPhone(new Phone("13245","212","Fijo"))
+                            .setPhone("23132132", "5445", "Cel")
+
+                            .setContact(new Contact("Daniel" , 
+                                new Phone("125454","54554","Fijo"),
+                                new Address("Av. Lima 4545" , "Lima" , "Peru" , "15663")))
+                            .build();
+
+        System.out.println("employee = " + employee);
+    }
+}
