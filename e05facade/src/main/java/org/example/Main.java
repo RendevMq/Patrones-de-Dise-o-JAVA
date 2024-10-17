e1c2e4d28ffb9aaea8f5e5d63298ea0f902bc1cd
RendevMq <rensystem73@gmail.com>
2024-10-16 23:37:00 -0500

    facade main

--- e05facade/src/main/java/org/example/Main.java
+++ e05facade/src/main/java/org/example/Main.java
@@ -1,7 +1,21 @@
 package org.example;
 
+import org.example.facade.BookingFacade;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BookingFacade bookingFacade = new BookingFacade();
+
+        String user = "FrequentFlyer123";
+        String password = "password123";
+        String from = "New York";
+        String to = "Los Angeles";
+        String flight = "AA101";
+        String flightClass = "Business";
+        String creditCard = "1234-5678-9876-5432";
+        double amount = 800.00;
+        String email = "customer@example.com";
+
+        bookingFacade.bookFlight(user, password, from, to, flight, flightClass, creditCard, amount, email);
     }
 }
